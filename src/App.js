5b4c384793a564d338f2ba6a638c9411e1340ef1
MildMax <joseph.s.burns93@gmail.com>
2022-01-17 12:37:02 -0500

    added description of first assignment, my name, course description

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Joseph Burns -- CS5610 Web Dev Assignment #1 (Setting Up a Web Development Environment)
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
