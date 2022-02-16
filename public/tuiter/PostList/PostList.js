32b82453d41934374ba4d13f0573513aff0dde55
MildMax <joseph.s.burns93@gmail.com>
2022-02-16 18:44:01 -0500

    implemented postlist array vals in home main content element

--- public/tuiter/PostList/PostList.js
+++ public/tuiter/PostList/PostList.js
@@ -2,58 +2,59 @@ const PostList = (post) => {
     return(`
         <li class="list-group-item wd-transparent-background-override">
             <div class="row">
-                <div class="wd-avatar-col ps-2 pe-0">
-                    <img class="wd-avatar-image wd-circle-border-radius" src="../images/leprechaun_avatar.png"/>
+                <div class="wd-avatar-col ps-3 pe-0">
+                    <img class="wd-avatar-image wd-circle-border-radius" src=${post.avatarIcon}/>
                 </div>
                 <div class="col wd-post-width-override">
-                    <div class="row ps-3 pe-2">
+                    <div class="row ps-3 pe-3">
                         <p class="mb-0 ps-0 col wd-name-handle-width-override wd-float-left wd-ellipsis-extended-text">
-                            <span class="wd-solid-white-text wd-bold-font">Robert Zubrin</span>
+                            <span class="wd-solid-white-text wd-bold-font">${post.userName}</span>
                             <i class="fa fa-check-circle"> </i>
-                            <span class="ms-1 wd-light-gray-color">@robert_zubrin - Jul 31</span>
+                            <span class="ms-1 wd-light-gray-color">${post.handle} - ${post.time}</span>
                         </p>
                         <a href="#" class="wd-align-text-center wd-light-gray-color wd-ellipses-line-height wd-ellipses-width ps-0 pe-0 wd-float-right">
                             <i class="fa fa-ellipsis-h float-right"> </i>
                         </a>
                     </div>
-                    <div class="row ps-3 pe-2">
+                    <div class="row ps-3 pe-3">
                         <p class=" ps-0 wd-solid-white-text wd-size-15-font wd-0-margin">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
+                            ${post.description}
                         </p>
                     </div>
-                    <div class="row ps-3 pe-2 wd-top-padding-12">
-                        <img  src="../images/classroom.jpg"
+                    <div class="row ps-3 pe-3 wd-top-padding-12">
+                        <img src=${post.mainImage}
                              class="ps-0 pe-0 img-fluid wd-article-image-border wd-article-image-border-radius wd-border-color-very-dark-grey">
                         <div class="wd-border-color-very-dark-grey wd-article-description-border-radius wd-padding-12 wd-0-margin wd-article-content-border-bottom">
-                            <div class="wd-solid-white-text wd-bold-font wd-ellipsis-extended-text wd-padding-right-100">Lorem ipsum
-                                dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua.</div>
-                            <p class="wd-0-margin wd-light-gray-color wd-margin-top-2">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <div class="wd-solid-white-text wd-bold-font wd-ellipsis-extended-text wd-padding-right-100">
+                                ${post.imageTitle}
+                            </div>
+                            <p class="wd-0-margin wd-light-gray-color wd-margin-top-2">
+                                ${post.imageDescription}
+                            </p>
                             <div class="wd-margin-top-2">
-                                <a href="#" class="wd-0-margin wd-light-gray-color wd-remove-link-underline">tuit.com</a>
+                                <a href="#" class="wd-0-margin wd-light-gray-color wd-remove-link-underline">
+                                    ${post.externalLink}
+                                </a>
                             </div>
                         </div>
                         <div class="row pt-2">
                             <div class="col-3 wd-icon-col-override wd-tuiter-font">
                                 <a href="#" class="wd-float-left wd-highlight-border-on-hover wd-remove-link-underline wd-light-gray-color">
                                     <i class="fa fa-icon-override fa-comment"></i>
-                                    <span class="ms-2">123</span>
+                                    <span class="ms-2">${post.commentCount}</span>
                                 </a>
                             </div>
                             <div class="col-3 wd-icon-col-override wd-tuiter-font">
                                 <a href="#" class="wd-float-left wd-remove-link-underline wd-light-gray-color">
                                     <i class="fa fa-retweet"></i>
-                                    <span class="ms-2">52</span>
+                                    <span class="ms-2">${post.retuitCount}</span>
                                 </a>
             
                             </div>
                             <div class="col-3 wd-icon-col-override wd-tuiter-font ">
                                 <a href="#" class="wd-float-left wd-highlight-border-on-hover wd-light-gray-color wd-remove-link-underline">
                                     <i class="fa fa-icon-override fa-heart"></i>
-                                    <span class="ms-2">123</span>
+                                    <span class="ms-2">${post.likeCount}</span>
                                 </a>
                             </div>
                             <div class="col-2 wd-tuiter-font">
--- public/tuiter/PostList/postitems.js
+++ public/tuiter/PostList/postitems.js
@@ -1,4 +1,4 @@
 export default [
-    {avatarIcon:'../images/nasa_log.png', userName: 'Joe', handle: 'MAMA', time: '2h', description: ' Yo mama', mainImage:'../images/classroom_2.jpg', imageTitle: 'MAMA', imageDescription: 'a picture of yo mama', externalLink: 'yomama.com'},
+    {avatarIcon:'../images/nasa_logo.png', userName: 'Joe', handle: 'MAMA', time: '2h', description: ' Yo mama', mainImage:'../images/classroom_2.jpg', imageTitle: 'MAMA', imageDescription: 'a picture of yo mama', externalLink: 'yomama.com', commentCount: 234, retuitCount: 69, likeCount: 234},
     {}
 ]
