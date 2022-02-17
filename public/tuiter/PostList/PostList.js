88b02525e77143a668b8f7843f5af884bd7b8ad0
MildMax <joseph.s.burns93@gmail.com>
2022-02-16 19:20:08 -0500

    fixed some styling

--- public/tuiter/PostList/PostList.js
+++ public/tuiter/PostList/PostList.js
@@ -46,28 +46,27 @@ const PostList = (post) => {
                             </div>
                         `
                         }
-                        
                         <div class="row pt-2">
-                            <div class="col-3 wd-icon-col-override wd-tuiter-font">
+                            <div class="col-3 ps-0 pe-0 wd-icon-col-override wd-tuiter-font wd-font-14">
                                 <a href="#" class="wd-float-left wd-highlight-border-on-hover wd-remove-link-underline wd-light-gray-color">
                                     <i class="fa fa-icon-override fa-comment"></i>
                                     <span class="ms-2">${post.commentCount}</span>
                                 </a>
                             </div>
-                            <div class="col-3 wd-icon-col-override wd-tuiter-font">
+                            <div class="col-3 ps-0 pe-0 wd-icon-col-override wd-tuiter-font wd-font-14">
                                 <a href="#" class="wd-float-left wd-remove-link-underline wd-light-gray-color">
                                     <i class="fa fa-retweet"></i>
                                     <span class="ms-2">${post.retuitCount}</span>
                                 </a>
             
                             </div>
-                            <div class="col-3 wd-icon-col-override wd-tuiter-font ">
+                            <div class="col-3 ps-0 pe-0 wd-icon-col-override wd-tuiter-font wd-font-14">
                                 <a href="#" class="wd-float-left wd-highlight-border-on-hover wd-light-gray-color wd-remove-link-underline">
                                     <i class="fa fa-icon-override fa-heart"></i>
                                     <span class="ms-2">${post.likeCount}</span>
                                 </a>
                             </div>
-                            <div class="col-2 wd-tuiter-font">
+                            <div class="col-2 ps-0 pe-0 wd-tuiter-font wd-line-height-1-point-4 wd-font-14">
                                 <a href="#" class="wd-remove-link-underline wd-light-gray-color wd-bold-font">
                                     <i class="fa fa-upload"></i>
                                 </a>
