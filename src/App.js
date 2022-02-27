960e73c18bdeeb14e13787d21f5361cd50e78524
MildMax <joseph.s.burns93@gmail.com>
2022-02-27 12:43:39 -0500

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+
+import {BrowserRouter,Route} from "react-router-dom";
+import HelloWorld from "./components/HelloWorld";
+import Labs from "./components/Labs";
+import Tuiter from "./components/Tuiter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Joseph Burns -- CS5610 Web Dev Assignment #1 (Setting Up a Web Development Environment)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="container">
+              <Route path="/hello">
+                  <HelloWorld/>
+              </Route>
+              <Route path="/labs">
+                  <Labs/>
+              </Route>
+              <Route path="/tuiter">
+                  <Tuiter/>
+              </Route>
+          </div>
+      </BrowserRouter>
   );
 }
 
 export default App;
+
+
