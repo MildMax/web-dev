e1437896dd2ec238d43e1b3cf7f47e2f80618caf
MildMax <joseph.s.burns93@gmail.com>
2022-03-08 12:25:41 -0500

    a6 React.js sp22

--- src/components/Tuiter/WhoToFollowList/index.js
+++ src/components/Tuiter/WhoToFollowList/index.js
@@ -5,7 +5,7 @@ import React from "react";
 const WhoToFollowList = () => {
     return (
         <ul className="list-group">
-            <li className="list-group-item padding-top-bottom-override">
+            <li className="list-group-item">
                 <p className="ms-0 me-0 mt-0 mb-0 wd-bold-font">Who to follow</p>
             </li>
             {
