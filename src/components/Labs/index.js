80a3d2f82ae00bb9bb10c0fa032c987aafd7def1
MildMax <joseph.s.burns93@gmail.com>
2022-03-09 12:15:03 -0500

    implemented a7 lab

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./ToDo/TodoList";
+import ReduxExamples from "./redux-examples";
 
 const Labs = () => {
+
+    const [hideA6, setHideA6] = useState(false);
+
+    const toggleHide = () => {
+        setHideA6(!hideA6);
+    }
+
     return(
         <>
             <h1>Labs</h1>
@@ -15,11 +23,29 @@ const Labs = () => {
             <Link to="/tuiter/home">
                 Tuiter
             </Link>
+            {/*a7 labs*/}
+            <ReduxExamples/>
+            {/*a6 labs*/}
+            <br/>
+            {(() => {
+                if (hideA6) {
+                    return (
+                        <button className="btn btn-primary btn-block rounded-pill" onClick={toggleHide}>Show A6 Labs</button>
+                    )
+                } else {
+                    return (
+                    <>
+                        <button className="btn btn-primary btn-block rounded-pill" onClick={toggleHide}>Hide A6 Labs</button>
+                        <br/>
+                        <br/>
+                        <ConditionalOutput/>
+                        <Styles/>
+                        <Classes/>
+                        <TodoList/>
+                    </>
+                );}
+            })()}
 
-            <ConditionalOutput/>
-            <Styles/>
-            <Classes/>
-            <TodoList/>
         </>
     )
 };
